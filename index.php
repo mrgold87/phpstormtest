e87f13c87f0c6b5e6315cedf4cfb41d9bca93c69
Dmitriy Barabanov <dmitriy.barabanov@gmail.comgit config --global user.name Dmitriy>
2021-04-06 21:53:46 +0300

    Первый комит

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+function balance($left, $right)
+{
+
+    switch (marksToDigit($left) <=> marksToDigit($right)) {
+        case -1:
+            return 'Right';
+            break;
+        case 1:
+            return 'Left';
+            break;
+        case 0:
+            return 'Balance';
+    };
+}
+
+function marksToDigit($str)
+{
+    $test = (substr_count($str, '!')) * 2 + (substr_count($str, '?')) * 3;
+    return $test ;
+}
+
+echo balance("!!", "??");
+echo '<br>';
+echo balance("!??", "?!!");
+echo '<br>';
+echo balance("!?!!", "?!?");
+echo '<br>';
+echo balance("!!???!????", "??!!?!!!!!!!");
